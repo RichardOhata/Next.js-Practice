84a45c49847cb73dce7807aa6c3f5007639a674c
RichardOhata <richardohata@gmail.com>
2023-05-01 23:10:59 -0700

    Created Next.js skeleton code

---
+++ my-app/src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+// import styles from "@/styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>WebDev Newz</title>
+        <meta name="keywords" content="web development, programming" />
+      </Head>
+      <h1>Welcome to Next</h1>
+    </div>
+  );
+}
