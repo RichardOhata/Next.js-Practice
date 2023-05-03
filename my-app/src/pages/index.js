78e9bc69945d5e1a3f9b8e2b94f3ba2146cdd1c3
RichardOhata <richardohata@gmail.com>
2023-05-03 11:24:42 -0700

    Finished app

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import { server } from "../../config/index";
 import ArticleList from "../../components/ArticleList";
 
 // import styles from "@/styles/Home.module.css";
@@ -7,19 +7,13 @@ export default function Home({ articles }) {
   console.log(articles);
   return (
     <div>
-      <Head>
-        <title>WebDev Newz</title>
-        <meta name="keywords" content="web development, programming" />
-      </Head>
       <ArticleList articles={articles} />
     </div>
   );
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?_limit=6`
-  );
+  const res = await fetch(`${server}/api/articles`);
   const articles = await res.json();
 
   return {
@@ -28,3 +22,16 @@ export const getStaticProps = async () => {
     },
   };
 };
+
+// export const getStaticProps = async () => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts?_limit=6`
+//   );
+//   const articles = await res.json();
+
+//   return {
+//     props: {
+//       articles,
+//     },
+//   };
+// };
