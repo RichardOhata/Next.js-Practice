2ec4029068d9076c4abdefb65bd34ddf7751d07a
RichardOhata <richardohata@gmail.com>
2023-05-02 19:49:40 -0700

    Added front end for articles

---
+++ my-app/components/ArticleItem.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import articleStyles from "../src/styles/Articles.module.css";
+
+const ArticleItem = ({ article }) => {
+  return (
+    <>
+      <Link href={`/article/${article.id}`} className={articleStyles.card}>
+        <h3>{article.title} &rarr;</h3>
+        <p>{article.body}</p>
+      </Link>
+    </>
+  );
+};
+
+export default ArticleItem;
